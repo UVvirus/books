9464180216a7fe719227e86f44f26a8579218a2d
yuvarajan <yuvasyuvarajan@gmail.com>
2020-09-10 11:11:19 +0530

    test

--- books/settings.py
+++ books/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     #created apps
     'users.apps.UsersConfig',
+    'pages.apps.PagesConfig',
 
 ]
 
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'books.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,3 +123,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 AUTH_USER_MODEL='users.Customuser'
+LOGIN_REDIRECT_URL='home'
+LOGOUT_REDIRECT_URL='home'
