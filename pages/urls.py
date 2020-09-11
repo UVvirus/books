886ab72791208cf89a6285332d35b8c345c1122a
yuvarajan <yuvasyuvarajan@gmail.com>
2020-09-11 13:16:58 +0530

    test1

---
+++ pages/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import Homepageview,Aboutpageview
+
+urlpatterns=[
+    path('about/',Aboutpageview.as_view(),name='about'),
+    path('',Homepageview.as_view(),name='home'),
+    ]
